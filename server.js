759c27bd0552bffed4e92f3fba99b060938eed1b
Sarel Kohen <kohen.s@husky.neu.edu>
2019-07-31 15:08:08 +0300

    Added new endpoints to the API

---
+++ server.js
@@ -0,0 +1,56 @@
+// Create express app
+var express = require("express")
+var app = express()
+var db = require("./database.js")
+
+// Server port
+var HTTP_PORT = 8000
+// Start server
+app.listen(HTTP_PORT, () => {
+    console.log("Server running on port %PORT%".replace("%PORT%",HTTP_PORT))
+});
+// Root endpoint
+app.get("/", (req, res, next) => {
+    res.json({"message":"Ok"})
+});
+
+// Insert here other API endpoints
+
+// Gets all users from database.
+app.get("/api/user", (req, res, next) => {
+    var sql = "select * from user"
+    var params = []
+    db.all(sql, params, (err, rows) => {
+        if (err) {
+            res.status(400).json({"error": err.message})
+            return
+        }
+        res.json({
+            "message": "success",
+            "data": rows
+        })
+    })
+})
+
+// Get a specific user by ID.
+app.get("/api/user/:id", (req, res, next) => {
+    var sql = "select * from user where id = ?"
+    var params = [req.params.id]
+    db.get(sql, params, (err,row) => {
+        if (err) {
+            res.status(400).json({"error":err.message})
+            return
+        }
+        res.json({
+            "message":"success",
+            "data":row
+        })
+    })
+})
+
+
+
+// Default response for any other request
+app.use(function(req, res){
+    res.status(404);
+});
